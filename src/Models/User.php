b32b3d3b8322c4f36eaeef4834806190b40d5d92
Jens Vercruysse <jensvercruysse@outlook.com>
2019-02-22 23:48:55 +0100

    added email and password checks

--- src/Models/User.php
+++ src/Models/User.php
@@ -7,4 +7,41 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     public $timestamps = false;
+
+    public function trim($item)
+    {
+        $trimmed = !empty($item) ? trim($item) : null;
+        return $trimmed;
+    }
+    
+    public function checkIfValidEmail($email)
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "Invalid email!";
+            return false;
+        }else  {
+            if($this->checkIfEmailExists($email)){
+                return !filter_var($email, FILTER_VALIDATE_EMAIL) === false;
+            }
+        }
+    }
+
+    public function checkIfEmailExists($email)
+    {
+        if (User::where('email', '=', $email)->exists()) {
+            echo "E-mail is already in use!";
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public function checkIfValidPassword($password, $passwordConfirmation)
+    {
+        if ($password == $passwordConfirmation) {
+            return password_hash($password, PASSWORD_BCRYPT);
+        }
+        echo "Passwords don't match!";
+        return "Passwords don't match!";
+    }
 }
