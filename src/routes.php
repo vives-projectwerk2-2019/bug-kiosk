f0858a3e22d60bb6696fa3727bdd8510dbc02432
Jens Vercruysse <jensvercruysse@outlook.com>
2019-02-23 22:40:46 +0100

    added routes for session

--- src/routes.php
+++ src/routes.php
@@ -12,7 +12,7 @@ $app->get('/index', function (Request $request, Response $response, array $args)
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
-$app->post('/login', function (Request $request, Response $response, array $args) {
+$app->post('/register', function (Request $request, Response $response, array $args) {
     $user = new User();
 
     if ($user->checkIfValidEmail($request->getParam('email')) ) {
@@ -22,7 +22,7 @@ $app->post('/login', function (Request $request, Response $response, array $args
     }
 
     if($user->checkIfValidPassword($request->getParam('password'), $request->getParam('confirmpassword'))) {
-        $user->password = password_hash($password, PASSWORD_BCRYPT);
+        $user->password = password_hash($request->getParam('password'), PASSWORD_BCRYPT);
     }else{
         return $this->renderer->render($response, 'register.phtml', $args);}
   
@@ -50,12 +50,37 @@ $app->get('/login', function (Request $request, Response $response, array $args)
     return $this->renderer->render($response, 'login.phtml', $args);
 });
 
+$app->post('/login', function (Request $request, Response $response, array $args) {
+    $loginUser = User::where('email', '=', $request->getParam('email'))->first();
+    if (password_verify($request->getParam('password'), $loginUser->password)) {
+        $this->session->set('user_id', $loginUser->id);
+
+        $args['user'] = $loginUser;
+        return $response->withRedirect('/home', $status = null);
+    } else {
+        return $response->withRedirect('/index', $status = null);
+    }
+});
+
 $app->get('/register', function (Request $request, Response $response, array $args) {
     return $this->renderer->render($response, 'register.phtml', $args);
 });
 
 $app->get('/home', function (Request $request, Response $response, array $args) {
+    $args['user'] = $request->getAttribute('user');
     return $this->renderer->render($response, 'home.phtml', $args);
 });
 
+$app->get('/editpw', function (Request $request, Response $response, array $args) {
+    return $this->renderer->render($response, 'editPassword.phtml', $args);
+});
 
+$app->get('/profile', function (Request $request, Response $response, array $args) {
+    $args['user'] = $request->getAttribute('user');
+    return $this->renderer->render($response, 'profile.phtml', $args);
+});
+
+$app->get('/logout', function (Request $request, Response $response, array $args) {
+    $this->session::destroy();
+    return $response->withRedirect('/login', $status = null);
+});
