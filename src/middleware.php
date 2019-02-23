c7ff2e469c147c7cb83bbef1a9a613a8e8b5a87d
Jens Vercruysse <jensvercruysse@outlook.com>
2019-02-23 22:40:14 +0100

    made a middleware function for login session

--- src/middleware.php
+++ src/middleware.php
@@ -1,4 +1,15 @@
 <?php
+use App\Models\User;
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+
+$app->add(function ($request, $response, $next) {
+
+    if ($this->session->get('user_id')) {
+        $this->user = User::find($this->session->get('user_id'));
+        $request = $request->withAttribute('user', $this->user);
+    }
+    $response = $next($request, $response);
+    return $response;
+});
