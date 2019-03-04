e0200e335306d348a4f938e1ff3c1031286a3c20
Jens Vercruysse <jensvercruysse@outlook.com>
2019-03-04 13:07:02 +0100

    added a layout scheme for pages and used it on index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>index</title>
-</head>
-<body>
-    hello world
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<div class="index">
+    <h1>Bug kiosk</h1>
+    <a  href="/register" class="waves-effect waves-light btn-large"><i class="material-icons right">person_add</i>Register</a>
+    <a  href="/login" class="waves-effect waves-light btn-large"><i class="material-icons right">lock_open</i>Login</a>
+</div>
+@endsection
