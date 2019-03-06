eacd927b13c77351f1c08284a42ed0b848ae2b5d
Jens Vercruysse <jensvercruysse@outlook.com>
2019-03-06 21:46:52 +0100

    added buttons for social media

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,5 +5,15 @@
     <h1>Bug kiosk</h1>
    <a  href="/register" class="waves-effect waves-light btn-large"><i class="material-icons right">person_add</i>Register</a>
     <a  href="/login" class="waves-effect waves-light btn-large"><i class="material-icons right">lock_open</i>Login</a>
+
+    <br>
+    <br>
+    <br>
+    <h4>Or continue with:</h4>
+    <br>
+    <a  href="/login/facebook" class="waves-effect waves-light btn-large">Facebook</a>
+    <a  href="/login/github" class="waves-effect waves-light btn-large">Github</a>
+    <a  href="#" class="waves-effect waves-light btn-large">Google</a>
+    <a  href="#" class="waves-effect waves-light btn-large">Twitter</a>
 </div>
 @endsection
