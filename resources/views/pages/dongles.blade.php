9dd94b1edcb927121d9df1f9aa18cede737208a5
Elise De Smet <elise.desmet99@gmail.com>
2019-03-13 15:13:27 +0100

    added title to pages

--- resources/views/pages/dongles.blade.php
+++ resources/views/pages/dongles.blade.php
@@ -3,5 +3,7 @@
 @include('inc.navbar')
 
 @section('content')
-
+<div>
+    <h1 id="under-navbar">All Dongles</h1>
+</div>
 @endsection
--- resources/views/pages/stats.blade.php
+++ resources/views/pages/stats.blade.php
@@ -3,5 +3,7 @@
 @include('inc.navbar')
 
 @section('content')
-
+<div>
+    <h1 id="under-navbar">Stats</h1>
+</div>
 @endsection
