313aaab99939afaeaab18de40df5f8b5435e005a
Elise De Smet <elise.desmet99@gmail.com>
2019-03-13 20:47:56 +0100

    added layout to pages

--- resources/views/pages/my_dongles.blade.php
+++ resources/views/pages/my_dongles.blade.php
@@ -4,6 +4,20 @@
 
 @section('content')
 <div>
-    <h1 id="under-navbar">My dongles</h1>
+    <h1 id="under-navbar">My Dongles</h1>
+        <div class="row">
+            <div id="dongle">Dongle 1
+                    <p>Name</p>
+                    <p>Info</p>
+            </div>
+            <div id="dongle">Dongle 2
+                    <p>Name</p>
+                    <p>Info</p>
+            </div>
+            <div id="dongle">Dongle 3
+                    <p>Name</p>
+                    <p>Info</p>
+            </div>
+        </div>
 </div>
 @endsection
--- resources/views/pages/stats.blade.php
+++ resources/views/pages/stats.blade.php
@@ -5,5 +5,10 @@
 @section('content')
 <div>
     <h1 id="under-navbar">Stats</h1>
+    <p>Username: </p>
+    <p>Level: </p>
+    <p>Game Quantity: </p>
+    <p>Recent Games: </p>
+    <p>...</p>
 </div>
 @endsection
