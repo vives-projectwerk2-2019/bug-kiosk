0c3b31460ca0ef28e46f149478813d519fbd15cc
Elise De Smet <elise.desmet99@gmail.com>
2019-03-14 19:02:38 +0100

    added dongles layout

--- resources/views/pages/my_dongles.blade.php
+++ resources/views/pages/my_dongles.blade.php
@@ -4,20 +4,23 @@
 
 @section('content')
 <div>
-    <h1 id="under-navbar">My Dongles</h1>
-        <div class="row">
-            <div id="dongle">Dongle 1
-                    <p>Name</p>
-                    <p>Info</p>
-            </div>
-            <div id="dongle">Dongle 2
-                    <p>Name</p>
-                    <p>Info</p>
-            </div>
-            <div id="dongle">Dongle 3
-                    <p>Name</p>
-                    <p>Info</p>
-            </div>
+<h1 id="under-navbar">All Dongles</h1>
+<div class="row">
+        <div class="dongle-white"></div>
+        <div class="dongle col s1">Dongle 1
+                <p>Name</p>
+                <p>Info</p>
         </div>
+        <div class="dongle-white"></div>
+        <div class="dongle col s1">Dongle 2
+                <p>Name</p>
+                <p>Info</p>
+        </div>
+        <div class="dongle-white"></div>
+        <div class="dongle col s1">Dongle 3
+                <p>Name</p>
+                <p>Info</p>
+        </div>
+        <div class="dongle-white"></div>
 </div>
 @endsection
