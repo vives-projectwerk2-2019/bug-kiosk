11f958f3c57a4c632c490b308de5459934701460
Elise De Smet <elise.desmet99@gmail.com>
2019-03-19 15:51:33 +0100

    added some layout

--- public/js/app.js
+++ public/js/app.js
@@ -52782,6 +52782,10 @@ var app = new vue__WEBPACK_IMPORTED_MODULE_0___default.a({
   },
   router: router
 });
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.tooltipped');
+  var instances = M.Tooltip.init(elems, options);
+});
 
 /***/ }),
 
--- resources/views/pages/dongles.blade.php
+++ resources/views/pages/dongles.blade.php
@@ -9,37 +9,58 @@
         <div class="dongle-white"></div>
         <div class="dongle col s1">Dongle 1
                 <p>Name</p>
-                <p>Info</p>
+                <a class="btn tooltipped" data-position="bottom" data-tooltip="Info dongle 1">Info</a>
         </div>
         <div class="dongle-white"></div>
         <div class="dongle col s1">Dongle 2
                 <p>Name</p>
-                <p>Info</p>
+                <a class="btn tooltipped" data-position="bottom" data-tooltip="Info dongle 2">Info</a>
         </div>
         <div class="dongle-white"></div>
         <div class="dongle col s1">Dongle 3
                 <p>Name</p>
-                <p>Info</p>
+                <a class="btn tooltipped" data-position="bottom" data-tooltip="Info dongle 3">Info</a>
         </div>
         <div class="dongle-white"></div>
     </div>
+    <br>
     <div class="row">
         <div class="dongle-white"></div>
         <div class="dongle col s1">Dongle 4
-                <p>Name</p>
-                <p>Info</p>
+                <p >Name</p>
+                <a class="btn tooltipped" data-position="bottom" data-tooltip="Info dongle 4">Info</a>
         </div>
         <div class="dongle-white"></div>
         <div class="dongle col s1">Dongle 5
                 <p>Name</p>
-                <p>Info</p>
+                <a class="btn tooltipped" data-position="bottom" data-tooltip="Info dongle 5">Info</a>
         </div>
         <div class="dongle-white"></div>
         <div class="dongle col s1">Dongle 6
                 <p>Name</p>
-                <p>Info</p>
+                <a class="btn tooltipped" data-position="bottom" data-tooltip="Info dongle 6">Info</a>
         </div>
         <div class="dongle-white"></div>
     </div>
+    <br>
+    <div class="row">
+        <div class="dongle-white"></div>
+        <div class="dongle col s1">Dongle 7
+                <p >Name</p>
+                <a class="btn tooltipped" data-position="bottom" data-tooltip="Info dongle 7">Info</a>
+        </div>
+        <div class="dongle-white"></div>
+        <div class="dongle col s1">Dongle 8
+                <p>Name</p>
+                <a class="btn tooltipped" data-position="bottom" data-tooltip="Info dongle 8">Info</a>
+        </div>
+        <div class="dongle-white"></div>
+        <div class="dongle col s1">Dongle 9
+                <p>Name</p>
+                <a class="btn tooltipped" data-position="bottom" data-tooltip="Info dongle 9">Info</a>
+        </div>
+        <div class="dongle-white"></div>
+    </div>
+        <br>
 </div>
 @endsection
