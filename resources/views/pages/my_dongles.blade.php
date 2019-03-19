9ba2b2436986a48ee560d940252ed632e1e51b72
Elise De Smet <elise.desmet99@gmail.com>
2019-03-19 17:39:10 +0100

    added tooltips to my_dongles file

--- resources/views/pages/my_dongles.blade.php
+++ resources/views/pages/my_dongles.blade.php
@@ -5,22 +5,17 @@
 @section('content')
 <div>
 <h1 id="under-navbar">All Dongles</h1>
-<div class="row">
-        <div class="dongle-white"></div>
-        <div class="dongle col s1">Dongle 1
-                <p>Name</p>
-                <p>Info</p>
-        </div>
-        <div class="dongle-white"></div>
-        <div class="dongle col s1">Dongle 2
-                <p>Name</p>
-                <p>Info</p>
-        </div>
-        <div class="dongle-white"></div>
-        <div class="dongle col s1">Dongle 3
-                <p>Name</p>
-                <p>Info</p>
-        </div>
-        <div class="dongle-white"></div>
-</div>
+<div class="row ">
+                <div class="dongle-white"></div>
+                <div class="dongle col s1 tooltipped" data-position="top" data-tooltip="Test test test test test">Dongle 1
+                </div>
+                <div class="dongle-white"></div>
+                <div class="dongle col s1 tooltipped" data-position="top" data-tooltip="Test test test test test">Dongle 2
+                </div>
+                <div class="dongle-white"></div>
+                <div class="dongle col s1 tooltipped" data-position="top" data-tooltip="Test test test test test">Dongle 3
+                </div>
+                <div class="dongle-white"></div>
+            </div>
+            <br>
 @endsection
