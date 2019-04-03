41d2d5671ac3cd3cb17d93144d6556d0bf15dfe6
Elise De Smet <elise.desmet99@gmail.com>
2019-04-03 20:17:36 +0200

    added icons to my_donles page

--- resources/views/pages/my_dongles.blade.php
+++ resources/views/pages/my_dongles.blade.php
@@ -5,17 +5,20 @@
 @section('content')
 <div>
 <h1 id="under-navbar">All Dongles</h1>
-<div class="row ">
-                <div class="dongle-white"></div>
-                <div class="dongle col s1 tooltipped" data-position="top" data-tooltip="Test test test test test">Dongle 1
-                </div>
-                <div class="dongle-white"></div>
-                <div class="dongle col s1 tooltipped" data-position="top" data-tooltip="Test test test test test">Dongle 2
-                </div>
-                <div class="dongle-white"></div>
-                <div class="dongle col s1 tooltipped" data-position="top" data-tooltip="Test test test test test">Dongle 3
-                </div>
-                <div class="dongle-white"></div>
-            </div>
-            <br>
+    <div class="row ">
+        <div class="dongle-white"></div>
+        <div class="col s1 tooltipped" data-position="top" data-tooltip="<a>Name: Life</a><br><br><a>Discription: Adds extra life</a>">
+        <img src="{{ asset('logo/life.png') }}" alt="Life icon" class="dongle-icon">
+        </div>
+        <div class="dongle-white"></div>
+        <div class="col s1 tooltipped" data-position="top" data-tooltip="<a>Name: Shield</a><br><br><a>Discription: Adds extra shield</a>">
+            <img src="{{ asset('logo/shield.png') }}" alt="Shield icon" class="dongle-icon">
+        </div>
+        <div class="dongle-white"></div>
+        <div class="col s1 tooltipped" data-position="top" data-tooltip="<a>Name: Life</a><br><br><a>Discription: Adds extra life</a>">
+            <img src="{{ asset('logo/life.png') }}" alt="Life icon" class="dongle-icon">
+        </div>
+        <div class="dongle-white"></div>
+    </div>
+<br>
 @endsection
