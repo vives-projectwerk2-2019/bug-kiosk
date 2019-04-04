ea405efce3211af704b91089500f095d088814d6
Jens Vercruysse <jensvercruysse@outlook.com>
2019-04-04 15:10:41 +0200

    added user_dongle_id to database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('user_dongle_id', 8)->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
             $table->string('provider_id')->nullable();
