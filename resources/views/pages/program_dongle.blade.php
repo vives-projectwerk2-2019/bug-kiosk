692c22cc5d34b7ccfcee4bd3bf9187ac47721444
Jop Bouckaert <jop.bouckaert@outlook.com>
2019-04-04 16:36:58 +0200

    generate random user_dongle_id

--- resources/views/pages/program_dongle.blade.php
+++ resources/views/pages/program_dongle.blade.php
@@ -3,6 +3,11 @@
 @include('inc.navbar')
 
 @section('content')
+<?php 
+$random = strval(bin2hex(openssl_random_pseudo_bytes(7)));
+$result = "00" . $random;
+?>
+
 <div id="dongles">
     <dongles uid={{Auth::user()->id}}></dongles>
 </div>
