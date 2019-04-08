b8ae896f2db7adc86dbdfd75a5fa48f9bf21dc1c
Jens Vercruysse <jensvercruysse@outlook.com>
2019-04-08 09:42:15 +0200

    fixed avatar_upload css

--- resources/views/pages/edit_profile.blade.php
+++ resources/views/pages/edit_profile.blade.php
@@ -7,13 +7,25 @@
 <h1 id="under-navbar">Profile</h1>
     <div class="avatar">
         <img src="{{ Auth::user()->avatar }}" alt="User icon" id="profile_picture">
-        <form enctype="multipart/form-data" action="/edit_profile" method="POST">
-            <label>Update profile picture.</label>
-            <input type="file" name="avatar" id="file_input">
-            @csrf
-            <input type="submit" class="pull-right btn btn-sm btn-primary" value="change">
-        </form>
-    </div>
+        <br>
+        <br>
+        <form action="/edit_profile" method="POST" enctype="multipart/form-data">
+            <div class="file-field input-field" id="avatar_input_field">
+              <div class="waves-effect waves-light btn-large">
+                <span>Select image</span>
+                <input type="file" name="avatar">
+                <i class="material-icons right">photo_library</i>
+              </div>
+              <div class="file-path-wrapper">
+                <input class="file-path validate" type="text" placeholder="Upload a custom avatar.">
+                @csrf
+              </div>
+              <button class="waves-effect waves-light btn-large" type="submit" name="action">Upload
+                <i class="material-icons right">file_upload</i>
+              </button>
+            </div>
+          </form>
+          <br>
     <div>
         <p id="profile-name">Name: {{ Auth::user()->name }}</p>
         <p id="profile-email">E-mail: {{ Auth::user()->email }}</p>
