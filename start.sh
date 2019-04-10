4fae8660dd3d3bd9a66da34a4cb7b03c5c013dab
Reavershark <jonasty1999@gmail.com>
2019-04-10 22:56:02 +0200

    Run db:seed in docker

--- start.sh
+++ start.sh
@@ -20,5 +20,6 @@ php artisan config:cache
 php artisan route:cache
 >&2 echo "Migrating to db"
 php artisan migrate --force
+php artisan db:seed --force
 >&2 echo "Serving laravel"
 php artisan serve --host=0.0.0.0
