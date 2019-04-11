a809001c68dd35a3d2e2e2317d5075f432144309
Jens Vercruysse <jensvercruysse@outlook.com>
2019-04-11 16:37:02 +0200

    fixed dongle generation css

--- resources/views/pages/dongles.blade.php
+++ resources/views/pages/dongles.blade.php
@@ -6,18 +6,17 @@
 
     <div class="container">
         <h1 id="under-navbar">All Dongles</h1>
-        <div id="left-logo-div">
         <?php $dongles = App\Dongle::All(); ?>
-
+        
         <div class="row">
             @foreach ($dongles as $dongle)
             <div class="col s6 m4 l3">
-                <div class="card">
+                <div class="card" id="dongle_card">
                     <div class="card-image waves-effect waves-block waves-light">
-                    <img src="{{ $dongle->logo }}"  style="object-fit: cover; width:230px; height:230px;">
+                    <img src="{{ $dongle->logo }}" id="dongles_img">
                     </div>
                     <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">{{$dongle->name}}<i class="material-icons right">more_vert</i></span>
+                        <span class="card-title activator grey-text text-darken-4" id="dongle_content">{{$dongle->name}}<i class="material-icons right">more_vert</i></span>
                     </div>
                     <div class="card-reveal">
                         <span class="card-title grey-text text-darken-4">{{$dongle->name}}<i class="material-icons right">close</i></span>
@@ -28,5 +27,4 @@
             @endforeach
         </div>
         </div>
-    </div>
 @endsection
--- resources/views/pages/my_dongles.blade.php
+++ resources/views/pages/my_dongles.blade.php
@@ -4,17 +4,30 @@
 
 @section('content')
 
-<div>
-<br>
-<h1 id="under-navbar">My Dongles</h1>
-<br>
-<br>
+<div class="container">
+    <br>
+    <h1 id="under-navbar">My Dongles</h1>
+    <br>
+    <br>
 
-<div id="left-logo-div">
     <?php $user = App\User::find(Auth::user()->id); ?>
-    @foreach ($user->dongles as $dongle)
-    <img src="{{ $dongle->logo }}" id="logo-list" class="tooltipped dongle-icon" data-position="top" data-tooltip="{{$dongle->name}}<br><br>{{$dongle->description}}">
-    @endforeach
-    <p id="stop-float">
+    <div class="row">
+        @foreach ($user->dongles as $dongle)
+        <div class="col s6 offset-s1" id="center_my_dongles">
+            <div class="card" id="my_dongle_card">
+                <div class="card-image waves-effect waves-block waves-light">
+                <img src="{{ $dongle->logo }}" id="my_dongle_img">
+                </div>
+                <div class="card-content">
+                    <span class="card-title activator grey-text text-darken-4" id="dongle_content"">{{$dongle->name}}<i class="material-icons right">more_vert</i></span>
+                </div>
+                <div class="card-reveal">
+                    <span class="card-title grey-text text-darken-4">{{$dongle->name}}<i class="material-icons right">close</i></span>
+                    <p>{{$dongle->description}}</p>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
 </div>
 @endsection
