32af3bb8d44d5c179748ba1bbcc94986b93314ec
Jens Vercruysse <jensvercruysse@outlook.com>
2019-04-11 13:06:24 +0200

    every new user is assigned the user role when creating an account

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Role;
 use App\User;
 
 class UserObserver
@@ -10,5 +11,11 @@ class UserObserver
     {
         $user->api_token = bin2hex(\openssl_random_pseudo_bytes(30));
         $user->user_dongle_id = "00" . strval(bin2hex(openssl_random_pseudo_bytes(7)));
+    }
+
+    public function created(User $user)
+    {
+        $role_id = Role::where('name', 'user')->first()->id;
+        $user->roles()->sync($role_id);
     }
 }
