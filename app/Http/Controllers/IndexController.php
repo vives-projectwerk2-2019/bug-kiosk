462edd4712ca0309cf08af150399b22e33fdb400
Jens Vercruysse <jensvercruysse@outlook.com>
2019-04-13 00:36:05 +0200

    users are now automatically redirected to home if session is stil active

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('pages.index');
+        if (Auth::check()) {
+            return redirect('/home');
+        } else {
+            return view('pages.index');
+        }
+
     }
 }
