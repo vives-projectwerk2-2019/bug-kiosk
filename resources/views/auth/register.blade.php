e8abe1ae5d0d2bddfbf4ff3174517eca478f10b5
Jens Vercruysse <jensvercruysse@outlook.com>
2019-04-13 00:20:54 +0200

    removed links from page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,8 +1,3 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link rel="stylesheet" type="text/css" media="screen" href="css/app.css" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-
 @extends('layouts.app')
 
 @section('content')
