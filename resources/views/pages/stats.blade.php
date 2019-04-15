e90a4abac46ea0d968a5b8c2e365c083c3c3f50c
Jens Vercruysse <jensvercruysse@outlook.com>
2019-04-15 22:05:06 +0200

    removed stats page

---
+++ resources/views/pages/stats.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@include('inc.navbar')
+
+@section('content')
+<div>
+    <h1 id="under-navbar">Stats</h1>
+    <p>Username: </p>
+    <p>Level: </p>
+    <p>Game Quantity: </p>
+    <p>Recent Games: </p>
+    <p>...</p>
+</div>
+@endsection
