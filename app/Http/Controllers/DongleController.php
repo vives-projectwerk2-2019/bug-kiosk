2def65e118c097abaa52bdf34173a7ae53029bdd
Jens Vercruysse <jensvercruysse@outlook.com>
2019-04-15 22:40:19 +0200

    prevented crash when activating less then 3 dongles at a time

--- app/Http/Controllers/DongleController.php
+++ app/Http/Controllers/DongleController.php
@@ -19,19 +19,32 @@ class DongleController extends Controller
 
         $body = json_decode($request->getContent(), true);
 
-        $dongle_id_1 = $body['dongle_id_1'];
-        $dongle_id_2 = $body['dongle_id_2'];
-        $dongle_id_3 = $body['dongle_id_3'];
+        $id_array = array();
 
-        $dongle_1 = Dongle::where('dongle_hash', $dongle_id_1)->first();
-        $dongle_1_id = $dongle_1->id;
-        $dongle_2 = Dongle::where('dongle_hash', $dongle_id_2)->first();
-        $dongle_2_id = $dongle_2->id;
-        $dongle_3 = Dongle::where('dongle_hash', $dongle_id_3)->first();
-        $dongle_3_id = $dongle_3->id;
+        if (isset($body['dongle_id_1'])) {
+            $dongle_id_1 = $body['dongle_id_1'];
+            $dongle_1 = Dongle::where('dongle_hash', $dongle_id_1)->first();
+            $dongle_1_id = $dongle_1->id;
+            array_push($id_array, $dongle_1_id);
+        }
+        if (isset($body['dongle_id_2'])) {
+            $dongle_id_2 = $body['dongle_id_2'];
+            $dongle_2 = Dongle::where('dongle_hash', $dongle_id_2)->first();
+            $dongle_2_id = $dongle_2->id;
+            array_push($id_array, $dongle_2_id);
+        }
+        if (isset($body['dongle_id_3'])) {
+            $dongle_id_3 = $body['dongle_id_3'];
+            $dongle_3 = Dongle::where('dongle_hash', $dongle_id_3)->first();
+            $dongle_3_id = $dongle_3->id;
+            array_push($id_array, $dongle_3_id);
+        }
 
-        $id_array = array($dongle_1_id, $dongle_2_id, $dongle_3_id);
-
-        $user->dongles()->sync($id_array);
+        if (empty($id_array)) {
+            return "Not a valid request!";
+        } else {
+            $user->dongles()->sync($id_array);
+            return "Active dongles updated succesfully!";
+        }
     }
 }
