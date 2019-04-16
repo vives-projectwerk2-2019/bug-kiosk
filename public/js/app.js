5fa03a6e8994f2b7967f8778665dc971a095aba0
Jop Bouckaert <jop.bouckaert@outlook.com>
2019-04-16 16:08:53 +0200

    update readme

--- public/js/app.js
+++ public/js/app.js
@@ -6851,8 +6851,8 @@ function config (options /*: ?DotenvConfigOptions */) /*: DotenvConfigOutput */ 
     const parsed = parse(fs.readFileSync(dotenvPath, { encoding }), { debug })
 
     Object.keys(parsed).forEach(function (key) {
-      if (!Object({"MIX_PUSHER_APP_CLUSTER":"","MIX_PUSHER_APP_KEY":"","MIX_VUE_APP_BROKER_HOST":"127.0.0.1:9001","NODE_ENV":"development"}).hasOwnProperty(key)) {
-        Object({"MIX_PUSHER_APP_CLUSTER":"","MIX_PUSHER_APP_KEY":"","MIX_VUE_APP_BROKER_HOST":"127.0.0.1:9001","NODE_ENV":"development"})[key] = parsed[key]
+      if (!Object({"MIX_PUSHER_APP_CLUSTER":"mt1","MIX_PUSHER_APP_KEY":"","MIX_VUE_APP_BROKER_HOST":"127.0.0.1:9001","NODE_ENV":"development"}).hasOwnProperty(key)) {
+        Object({"MIX_PUSHER_APP_CLUSTER":"mt1","MIX_PUSHER_APP_KEY":"","MIX_VUE_APP_BROKER_HOST":"127.0.0.1:9001","NODE_ENV":"development"})[key] = parsed[key]
       } else if (debug) {
         log(`"${key}" is already defined in \`process.env\` and will not be overwritten`)
       }
@@ -53746,8 +53746,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\jensv\OneDrive\Documenten\School\Tweede jaar\Projectwerk\bug-kiosk\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\jensv\OneDrive\Documenten\School\Tweede jaar\Projectwerk\bug-kiosk\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\jopbo\OneDrive - Hogeschool VIVES\Vives\Projectwerk_2\bug-kiosk\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\jopbo\OneDrive - Hogeschool VIVES\Vives\Projectwerk_2\bug-kiosk\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- readme.md
+++ readme.md
@@ -27,7 +27,7 @@ There hasn't been any traffic yet.
 ## Ready
 <img src="public/images/kiosk/add-user.png" alt="init" width="200"/>
 
-The station sends `{"ping":"ping"}` on the topic `{kiosk/pi1/ping}` and is resend within 30 seconds.
+The station sends `{"ping":"ping"}` on the topic `kiosk/pi1/ping` and is resend within 30 seconds.
 
 ## No connection
 <img src="public/images/kiosk/sad.png" alt="init" width="200"/>
@@ -37,16 +37,16 @@ No ping from station for more than 30 seconds.
 ## Busy
 <img src="public/images/kiosk/waiting.png" alt="init" width="200"/>
 
-The station sends `{"status":"busy"}` on the topic `{kiosk/pi1/status}`.
+The station sends `{"status":"busy"}` on the topic `kiosk/pi1/status`.
 
 ## Done
 <img src="public/images/kiosk/add-contact.png" alt="init" width="200"/>
 
-The station sends `{"status":"ack"}` on the topic `{kiosk/pi1/status}`.
+The station sends `{"status":"ack"}` on the topic `kiosk/pi1/status`.
 
 ## Error
 <img src="public/images/kiosk/error.png" alt="init" width="200"/>
 
-The station sends `{"status":"nack"}` on the topic `{kiosk/pi1/status}`.
+The station sends `{"status":"nack"}` on the topic `kiosk/pi1/status`.
 
 
