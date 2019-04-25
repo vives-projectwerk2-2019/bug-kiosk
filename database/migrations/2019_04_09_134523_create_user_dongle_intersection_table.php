4ea3d3c044357552fd6d3d31e8660d7934d75154
Jens Vercruysse <jensvercruysse@outlook.com>
2019-04-25 10:24:03 +0200

    made it possible to delete dongles

--- database/migrations/2019_04_09_134523_create_user_dongle_intersection_table.php
+++ database/migrations/2019_04_09_134523_create_user_dongle_intersection_table.php
@@ -16,9 +16,9 @@ class CreateUserDongleIntersectionTable extends Migration
         Schema::create('user_dongle_intersection', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->BigInteger('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onCascade('delete');;
             $table->BigInteger('dongle_id')->unsigned();
-            $table->foreign('dongle_id')->references('id')->on('dongles');
+            $table->foreign('dongle_id')->references('id')->on('dongles')->onCascade('delete');;
         });
     }
 
