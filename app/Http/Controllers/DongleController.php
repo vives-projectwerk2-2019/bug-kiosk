1a18d719c9eb51a99e9d0799eb29fee4999e9e7b
Jens Vercruysse <jensvercruysse@outlook.com>
2019-04-26 16:21:28 +0200

    prevented crash when sending request with invalid user dongle id

--- app/Http/Controllers/DongleController.php
+++ app/Http/Controllers/DongleController.php
@@ -15,47 +15,51 @@ class DongleController extends Controller
 
     public function activate(Request $request, $user_dongle_id)
     {
-        $user = User::where('user_dongle_id', $user_dongle_id)->first();
-
-        $body = json_decode($request->getContent(), true);
-
-        $id_array = array();
-
-        $emptyCheck = false;
-
-        if (isset($body['dongle_id_1']) && !empty($body['dongle_id_1'])) {
-            $dongle_id_1 = $body['dongle_id_1'];
-            $dongle_1 = Dongle::where('dongle_hash', $dongle_id_1)->first();
-            $dongle_1_id = $dongle_1->id;
-            array_push($id_array, $dongle_1_id);
-        } else if (isset($body['dongle_id_1'])) {
-            $user->dongles()->sync($id_array);
-            $emptyCheck = true;
-        }
-        if (isset($body['dongle_id_2']) && !empty($body['dongle_id_2'])) {
-            $dongle_id_2 = $body['dongle_id_2'];
-            $dongle_2 = Dongle::where('dongle_hash', $dongle_id_2)->first();
-            $dongle_2_id = $dongle_2->id;
-            array_push($id_array, $dongle_2_id);
-        } else if (isset($body['dongle_id_2'])) {
-            $user->dongles()->sync($id_array);
-            $emptyCheck = true;
-        }
-        if (isset($body['dongle_id_3']) && !empty($body['dongle_id_2'])) {
-            $dongle_id_3 = $body['dongle_id_3'];
-            $dongle_3 = Dongle::where('dongle_hash', $dongle_id_3)->first();
-            $dongle_3_id = $dongle_3->id;
-            array_push($id_array, $dongle_3_id);
-        } else if (isset($body['dongle_id_3'])) {
-            $user->dongles()->sync($id_array);
-            $emptyCheck = true;
-        }
-
-        if (empty($id_array) && $emptyCheck == false) {
-            return "Not a valid request!";
+        if (User::where('user_dongle_id', $user_dongle_id)->first() == null) {
+            return "Not a valid user_dongle_id!";
         } else {
-            $user->dongles()->sync($id_array);
-            return "Active dongles updated succesfully!";
+            $user = User::where('user_dongle_id', $user_dongle_id)->first();
+
+            $body = json_decode($request->getContent(), true);
+
+            $id_array = array();
+
+            $emptyCheck = false;
+
+            if (isset($body['dongle_id_1']) && !empty($body['dongle_id_1'])) {
+                $dongle_id_1 = $body['dongle_id_1'];
+                $dongle_1 = Dongle::where('dongle_hash', $dongle_id_1)->first();
+                $dongle_1_id = $dongle_1->id;
+                array_push($id_array, $dongle_1_id);
+            } else if (isset($body['dongle_id_1'])) {
+                $user->dongles()->sync($id_array);
+                $emptyCheck = true;
+            }
+            if (isset($body['dongle_id_2']) && !empty($body['dongle_id_2'])) {
+                $dongle_id_2 = $body['dongle_id_2'];
+                $dongle_2 = Dongle::where('dongle_hash', $dongle_id_2)->first();
+                $dongle_2_id = $dongle_2->id;
+                array_push($id_array, $dongle_2_id);
+            } else if (isset($body['dongle_id_2'])) {
+                $user->dongles()->sync($id_array);
+                $emptyCheck = true;
+            }
+            if (isset($body['dongle_id_3']) && !empty($body['dongle_id_2'])) {
+                $dongle_id_3 = $body['dongle_id_3'];
+                $dongle_3 = Dongle::where('dongle_hash', $dongle_id_3)->first();
+                $dongle_3_id = $dongle_3->id;
+                array_push($id_array, $dongle_3_id);
+            } else if (isset($body['dongle_id_3'])) {
+                $user->dongles()->sync($id_array);
+                $emptyCheck = true;
+            }
+
+            if (empty($id_array) && $emptyCheck == false) {
+                return "Not a valid request!";
+            } else {
+                $user->dongles()->sync($id_array);
+                return "Active dongles updated succesfully!";
+            }
         }
     }
 }
