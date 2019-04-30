05c3a6bf93ae9e66630826634d5ad065b045e2d6
Jens Vercruysse <jensvercruysse@outlook.com>
2019-04-30 16:03:03 +0200

    fixed margin on profile page

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div>
-    <h1 id="under-navbar" style="margin:0;">Profile</h1>
+    <h1 id="under-navbar">Profile</h1>
     <div id="generator">
         <generator uid={{Auth::user()->user_dongle_id}} name={{Auth::user()->name}} img="{{ Auth::user()->avatar }}"></generator>
     </div>
@@ -11,7 +11,7 @@
         <p id="profile-name">Name: {{ Auth::user()->name }}</p>
         <p id="profile-email">E-mail: {{ Auth::user()->email }}</p>
         <p id="user_dongle_id">User dongle id: {{ Auth::user()->user_dongle_id }}</p>
-        <a  href="edit_profile" class="waves-effect waves-light btn-large" id="profile-button" style="margin: 0;"><i class="material-icons right">build</i>Edit</a>
+        <a  href="edit_profile" class="waves-effect waves-light btn-large" id="profile-button"><i class="material-icons right">build</i>Edit</a>
     </div>
 </div>
 @endsection
