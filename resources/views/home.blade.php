ab912806460bb1cec4a2e6be1dba5cae24a82ead
Elise De Smet <elise.desmet99@gmail.com>
2019-04-30 14:24:28 +0200

    changed kiosk home text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,28 +23,36 @@
 
             <p>
                 Welcome to the kiosk for BUG: Bug Universal Gamecontroller!
-                BUG is a portable gamecontroller with the focus on low power. 
-                Which means that the focus is on a controller with a long batterie life-time. 
             </p>
             <p>
-                Together with the controller, you can add 4 dongles. 
-                with 3 of them are add-ons where you can modify your playstyle. 
-                Thanks to the kiosk, you can choose the function of each add-on for the playstyle you wanna play.
+                BUG is the worlds most low power game console. 
+                By using the LoRaWAN Internet of Things wireless technology, 
+                BUG is able to communicate over long distances without using mucht power. 
+                Therefore you won't need to think about batteries in the controller.
             </p>
             <p>
-                To chose the Functions for the dongles:
+                You can add 4 dongles to the controller:  
                 <ul>
-                    <li> First, go to the menu right above. </li>
-                    <li> Then, chose 'Program Dongles'. </li>
+                    <li>
+                        One of the dongles is used to identify the player of the controller. 
+                        This way, anyone could get a personal dongle. 
+                        If you would like to play with any controller you just need to insert your personal dongle, 
+                        and the game knows who you are.
+                    </li>
+                    <li>
+                        The other 3 dongles are used to modify your game. 
+                        Depending on the dongles you insert in your controller you could make personalize your tank.
+                    </li>
                 </ul>
             </p>
             <p>
-                You will come to a menu, where you have the option to chose the functions for each dongle.
-                Chose with what you wanna play and press on the button below.
+                The Kiosk provides some information about the add-ons. 
+                You can choose the function of each add-on. To program your dongle, you can go to kiosk home page.
+                In the menu right above, you can chose for 'Program Dongles'. Here you can chose the functions you want to play with.
+                When you have chosen the functions you want, just click the button below.
             </p>
             <p>
-                If you want a view of what for types we have, 
-                go to Dongles and you will see all the type of add-ons/dongles you can use to utilize your playstyle.
+                You can find a list of all fuctions for the dongles in the Kiosk under "Dongles".
             </p>
         </div>
     </div>
