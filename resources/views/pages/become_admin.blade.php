8aaa22b4051c3e2be781ab0c61f0d0ad0cf682a5
Jens Vercruysse <jensvercruysse@outlook.com>
2019-05-02 23:39:44 +0200

    rewrote become_admin in materialze

--- resources/views/pages/become_admin.blade.php
+++ resources/views/pages/become_admin.blade.php
@@ -2,43 +2,30 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Admin registration</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <h1 style="margin-bottom: 10%">Become admin</h1>
 
-                    @if(session()->has('error'))
-                    <p style="font-weight: bold; color: red;">{{ session()->get('error') }}</p>
-                    @else
-                    Please enter the admin key.
-                    @endif
+        <div class="row">
+    @if(session()->has('error'))
+    <p style="font-weight: bold; color: red;">{{ session()->get('error') }}</p>
+    @else
+    <p class="col s6 offset-s3">Please enter the admin key.</p>
+    @endif
+        </div>
 
-                    <div class="row">
-                        <form class="col s12"method="POST" action="/become_admin">
-                            @csrf
-                          <div class="row">
-                            <div class="input-field col s6">
-                              <i class="material-icons prefix">security</i>
-                              <input id="icon_prefix" name="admin_key" type="text" class="validate" required="required">
-                              <label for="icon_prefix">Admin key</label>
-                            </div>
-                          </div>
-                          <button class="waves-effect waves-light btn-large" type="submit" name="action">Submit
-                                <i class="material-icons right">send</i>
-                            </button>
-                        </form>
-                      </div>
-                
-                </div>
+    <div class="row"">
+        <form class="col s12"method="POST" action="/become_admin">
+            @csrf
+        <div class="row">
+            <div class="input-field col s6 offset-s3">
+            <i class="material-icons prefix">security</i>
+            <input id="icon_prefix" name="admin_key" type="text" class="validate" required="required">
+            <label for="icon_prefix">Admin key</label>
             </div>
         </div>
+        <button class="waves-effect waves-light btn-large" type="submit" name="action">Submit admin key
+                <i class="material-icons right">send</i>
+            </button>
+        </form>
     </div>
-</div>
 @endsection
