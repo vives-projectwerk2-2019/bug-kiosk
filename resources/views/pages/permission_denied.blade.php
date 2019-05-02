88e81b8c9f3de6f0389288206a6d544e387cba31
Jens Vercruysse <jensvercruysse@outlook.com>
2019-05-02 23:31:35 +0200

    rewrote permission denied view in materialize

--- resources/views/pages/permission_denied.blade.php
+++ resources/views/pages/permission_denied.blade.php
@@ -2,23 +2,19 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Permission denied!</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <h1>Permission denied!</h1>
 
-                    You do not have permission to view the admin page, if you think this is a mistake please contact an administrator.
-                </div>
-            </div>
-        </div>
+    <br>
+    <br>
+
+    <div class="row">
+        <p class="col s6 offset-s3" style="border: 2px solid red"><br>You do not have permission to view the admin page! <br> If you think this is a mistake please contact an administrator.<br>...</p>
     </div>
+        
+    <br>
+    <br>
+    <br>
     <a  href="home" class="waves-effect waves-light btn-large">Return to home</a>
 </div>
 @endsection
