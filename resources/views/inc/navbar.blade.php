5a9123829aa5dc62e6d7efd1e637b772a34baba7
Jens Vercruysse <jensvercruysse@outlook.com>
2019-05-03 11:58:27 +0200

    hover now covers profile picture and picture is clickable

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -21,13 +21,13 @@
             <li><a style="color:rgba(0, 0, 0, 0.5);" href="/program_dongle">Program dongle</a></li>
             <li><a style="color:rgba(0, 0, 0, 0.5);" href="/dongles">Dongles</a></li>
             <li><a style="color:rgba(0, 0, 0, 0.5);" href="/logger">Logger</a></li>
-            <li><img style="margin-top: 6px;" src="{{ asset(Auth::user()->avatar) }}" id="small_avatar" width="0" height="0"></li>
-            <li>
-                <a style="color:rgba(0, 0, 0, 0.5);" class="dropdown-trigger" href="" data-target="dropdown1">
+            <li class="dropdown-trigger" data-target="dropdown1">
+                <a style="color:rgba(0, 0, 0, 0.5); float:right" >
+              <img style="6px; float:left;" src="{{ asset(Auth::user()->avatar) }}" id="small_avatar">
                         @if (Auth::User()->roles->pluck('name')->contains('admin'))
-                            Admin: {{ Auth::user()->name }} <span class="caret"></span>
+                        &nbsp; Admin: {{ Auth::user()->name }} <span class="caret"></span>
                         @else 
-                            {{ Auth::user()->name }} <span class="caret"></span>
+                        &nbsp; {{ Auth::user()->name }} <span class="caret"></span>
                         @endif
                     <i class="material-icons right">arrow_drop_down</i>
                 </a>
