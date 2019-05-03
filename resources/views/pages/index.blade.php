bb1853188aa96193bd16bd040cbb12c9cc2210f4
Jens Vercruysse <jensvercruysse@outlook.com>
2019-05-03 02:29:45 +0200

    fixed css bug on index page when using pixel 2

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+a {
+    margin-bottom: 5%;
+}
+</style>
 <div class="index">
     <h1>Bug kiosk</h1>
    <a  href="/register" class="waves-effect waves-light btn-large"><i class="material-icons right">person_add</i>Register</a>
@@ -11,9 +16,9 @@
     <br>
     <h4>Or continue with:</h4>
     <br>
-    <a  href="/login/facebook" class="waves-effect waves-light btn-large">Facebook</a>
-    <a  href="/login/github" class="waves-effect waves-light btn-large">Github</a>
-    <a  href="/login/google" class="waves-effect waves-light btn-large">Google</a>
-    <a  href="/login/twitter" class="waves-effect waves-light btn-large">Twitter</a>
+    <a href="/login/facebook" class="waves-effect waves-light btn-large">Facebook</a>
+    <a href="/login/github" class="waves-effect waves-light btn-large">Github</a>
+    <a href="/login/google" class="waves-effect waves-light btn-large">Google</a>
+    <a href="/login/twitter" class="waves-effect waves-light btn-large">Twitter</a>
 </div>
 @endsection
